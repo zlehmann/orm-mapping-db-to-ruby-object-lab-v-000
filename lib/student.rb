0ea15152106a0cd53f8d2cb1ee1956b70cade8d0
Zachary Lehmann <zlehmann@gmail.com>
2018-11-06 02:13:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,10 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = Student.new(row[1], row[2])
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
   end
 
   def self.all
@@ -14,6 +17,16 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
